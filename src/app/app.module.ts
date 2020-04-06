88f88ab5609bcf89c81c17a218a4410df1fa1b85
Felix Alejandro Tonatiuh Gomez Briones <tonatiuh.gomez@hp.com>
2020-04-05 22:45:26 -0500

    Adding arquitecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddComponent } from './components/add/add.component';
+import { ListComponent } from './components/list/list.component';
+import { EditComponent } from './components/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ListComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule
