d7e7fca2e7afba9df33f6455c47aba267fb376c1
Felix Alejandro Tonatiuh Gomez Briones <tonatiuh.gomez@hp.com>
2020-04-05 23:17:20 -0500

    delete from List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {without} from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'formAddEditDeleteAngular';
+export class AppComponent implements OnInit {
+  title = 'Coding Challenge';
+  theList: object[];
+
+  deletePaymentItem(thePayment: object){
+    this.theList = without(this.theList, thePayment);
+  }
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<Object[]>("../assets/data.json").subscribe(data => {
+      console.log(data);
+      this.theList = data;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AddComponent } from './components/add/add.component';
@@ -14,7 +15,8 @@ import { EditComponent } from './components/edit/edit.component';
     EditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
